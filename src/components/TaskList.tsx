74097d7e9057863ea806ebeae3d429258de53e50
Babi <barbara.ns@outlook.com>
2022-06-18 17:56:09 -0300

    Adiona funcionalidade de add uma nova task, marcar como feita e excluir.

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -15,15 +15,27 @@ export function TaskList() {
   const [newTaskTitle, setNewTaskTitle] = useState('');
 
   function handleCreateNewTask() {
-    // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    if (newTaskTitle) {
+      const idRandom = Math.floor(Date.now() * Math.random())
+      const newTask = { id: idRandom, title: newTaskTitle, isComplete: false }
+
+      setTasks([...tasks, newTask])
+      setNewTaskTitle('')
+    }
   }
 
   function handleToggleTaskCompletion(id: number) {
-    // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    const newTasks = tasks.map(task => ({
+        ...task,
+        isComplete: task.id === id ? !task.isComplete : task.isComplete
+    }))
+
+    setTasks(newTasks)
   }
 
   function handleRemoveTask(id: number) {
-    // Remova uma task da listagem pelo ID
+    const newTasks = tasks.filter(task => task.id !== id)
+    setTasks(newTasks)
   }
 
   return (
